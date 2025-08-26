e8e848a43ea9be0394be69054ed793c1c20401d9
Mike <mclancy96@gmail.com>
2025-08-26 12:53:12 -0400

    Update to rails 7.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,10 +40,8 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
 
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  # Asset pipeline configs removed for Rails 7.1 compatibility.
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,9 +27,10 @@ require 'capybara/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
